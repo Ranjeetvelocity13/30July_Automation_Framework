cdfbae76b892f0d32cc44c506338c9f4fbd7e929
Ranjeetvelocity13 <108998551+Ranjeetvelocity13@users.noreply.github.com>
2023-01-26 07:06:09 +0530

    Update Demo.java

--- 30July_Automation_Framework/src/main/java/Com/Ecommerce/PageClass/Demo.java
+++ 30July_Automation_Framework/src/main/java/Com/Ecommerce/PageClass/Demo.java
@@ -5,7 +5,8 @@ public class Demo {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello");
-	        System.out.println("Hi");
+	        System.out.println("Hello");
+		
 
 
 	}
